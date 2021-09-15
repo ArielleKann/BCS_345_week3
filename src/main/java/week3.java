e3998190c450d825dfaace1734079c8f656193ac
Arielle Kann <kannab@farmingdale.edu>
2021-09-15 10:19:29 -0400

    initial

---
+++ src/main/java/week3.java
@@ -0,0 +1,14 @@
+
+public class week3 {
+    
+   static int getSum(int a, int b){ //parameter a,b
+       return a+b;
+   }
+    public static void main(String[] args){
+        int x=10, y=100; //arguements
+        int z= getSum(x,y);
+        System.out.println("The sum is "+ z);
+    
+    }
+    
+}
