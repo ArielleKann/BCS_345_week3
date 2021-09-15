9d29c1ecc47a0230407c3140341d78031af2cae5
Arielle Kann <kannab@farmingdale.edu>
2021-09-15 10:35:36 -0400

    initial

--- src/main/java/week3.java
+++ src/main/java/week3.java
@@ -1,14 +1,18 @@
 
 public class week3 {
+    //lab 3 class:create a method that takes a string and returns how many
+    //Characters are there.
+    //Csll this method and pass your name as an arguement.
     
-   static int getSum(int a, int b){ //parameter a,b
-       return a+b;
-   }
+    
+    int getStrLngth(String name){
+        int l= name.length();
+        return l;}
     public static void main(String[] args){
-        int x=10, y=100; //arguements
-        int z= getSum(x,y);
-        System.out.println("The sum is "+ z);
-    
+        NewClass nc = new NewClass();
+        String str= "Arielle Kann";
+        int z= nc.getStrLngth(str);
+        System.out.println("The length is " + z);
     }
-    
-}
+    }
+   
